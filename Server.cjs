64fbd43adb637ca238580e4eefc30be82efdc890
aakashdeshamne <21512493.dypit@dypvp.edu.in>
2025-01-02 16:32:00 +0530

    Wrtitng restfull-Apis

--- Server.cjs
+++ Server.cjs
@@ -1,5 +1,42 @@
 const express = require('express')
-const app = express()
-var test;
-app.all('/', (req, res) => res.send(test));
+const app = express();
+const Books = [
+  {
+    title: "The Catcher in the Rye",
+    author: "J.D. Salinger",
+    year: 1951,
+  },
+  {
+    title: "To Kill a Mockingbird",
+    author: "Harper Lee",
+    year: 1960,
+  },
+
+  {
+    title: "1984",
+    author: "George Orwell",
+    year: 1949,
+  },
+
+  {
+    title: "The Great Gatsby",
+    author: "F. Scott Fitzgerald",
+    year: 1925,
+  },
+
+  {
+    title: "Pride and Prejudice",
+    author: "Jane Austen",
+    year: 1813,
+  },
+];
+
+app.get('/books', async(req, res) => {
+   try{
+         res.json(Books);
+   } catch (error) {
+         res.status(500).send('Error fetching books');
+    }
+});
+    
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
